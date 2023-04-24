1f76a3a158703058f6f55e95985eb3e945df46a8
Payam Karbassi <pkarbassi@hotmail.co.uk>
2023-04-24 13:58:40 +0100

    Implement createquestionandanswer page

---
+++ ITCWeek4SwiftUIFriday/ITCWeek4SwiftUIFriday/View/CreateQuestionAndAnswerScreen.swift
@@ -0,0 +1,48 @@
+//
+//  CreateQuestionAndAnswerScreen.swift
+//  ITCWeek4SwiftUIFriday
+//
+//  Created by Payam Karbassi on 24/04/2023.
+//
+
+import SwiftUI
+
+struct CreateQuestionAndAnswerScreen: View {
+    var ques : String = ""
+    @Binding var isShown : Bool
+    @Binding var myQuestion : String
+    @Binding var myAnswer : String
+    
+    var body: some View {
+            VStack{
+                HStack{
+                    Button {
+                        isShown.toggle()
+                    } label: {
+                        Text("X")
+                            .foregroundColor(.black)
+                            .font(.largeTitle)
+                            .padding()
+                    }
+                    Spacer()
+                }
+                
+                Group{
+                    TextField("Write your question here ...", text: $myQuestion).textFieldStyle(.roundedBorder)
+                    
+                    TextField("Write you answer here ...", text: $myAnswer).textFieldStyle(.roundedBorder)
+                }.padding()
+                
+                Spacer()
+            }
+    }
+    
+}
+
+struct CreateQuestionAndAnswerScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateQuestionAndAnswerScreen(isShown: .constant(true)
+                                      ,myQuestion: .constant(""),
+                                      myAnswer: .constant(""))
+    }
+}
--- ITCWeek4SwiftUIFriday/ITCWeek4SwiftUIFriday/View/QuestionAndAnswersScreen.swift
+++ ITCWeek4SwiftUIFriday/ITCWeek4SwiftUIFriday/View/QuestionAndAnswersScreen.swift
@@ -9,16 +9,31 @@ import SwiftUI
 
 struct QuestionAndAnswersScreen: View {
     @State var answer:String = ""
+    @State var givenAnswer : String = ""
+    @State var question :String = "What is 4 * 4 ?"
     let imageUrl = "https://media.istockphoto.com/id/1413840933/photo/old-books-on-wooden-shelf-tiled-bookshelf-background-concept-on-the-theme-of-history.jpg?b=1&s=612x612&w=0&k=20&c=wSw2dBYfChbj5EFgd8xcxe3lJWLjLeMvfHSdOqNOVEs="
+    
+    @State var createQuestionPageIsShown = false
+    
     var body: some View {
         //ScrollView{
             VStack(spacing: 5){
+                HStack{
+                    Spacer()
+                    Button {
+                        createQuestionPageIsShown = true
+                    } label: {
+                        Text("+").font(.largeTitle)
+                    }.padding(.trailing,10)
+                }
                 
-                Text("What is 4 * 4 asdkfla lasd lsjdf alkasjd lkasjdf asdkjfalsdf  ?").frame(maxWidth: .infinity,alignment: .leading).padding([.leading,.trailing],2)
+                Text(question).frame(maxWidth: .infinity,alignment: .leading).padding([.leading,.trailing],2)
                 
                 ZStack{
                     AsyncImage(url: URL(string: imageUrl)) { image in
-                        image.resizable().aspectRatio( contentMode: .fill).frame(maxWidth: .infinity, maxHeight: 300)
+                        image.resizable()
+                            .aspectRatio( contentMode: .fill)
+                            .frame(maxWidth: .infinity, maxHeight: 300)
                     } placeholder: {
                         ProgressView()
                     }
@@ -32,22 +47,30 @@ struct QuestionAndAnswersScreen: View {
                     
                 }.frame(maxWidth: .infinity,alignment: .leading).padding([.leading,.trailing],2)
                 
-                TextField("Type your Answer..", text: $answer)
+                TextField("Type your Answer..", text: $givenAnswer)
                     .textFieldStyle(.roundedBorder)
-                    .padding([.leading,.trailing],16)
+                    .padding([.leading,.trailing],2)
                 
                 Button {
-                    print(answer)
+                    if givenAnswer == answer {
+                        print("Correct")
+                    }else {
+                        print("Wrong")
+                    }
                 } label: {
                     Text("Submit")
-                        .frame(maxWidth: .infinity,maxHeight: 40)
+                        .frame(maxWidth: .infinity,maxHeight: 45)
                         .buttonStyle(.borderedProminent)
                         .background(.mint)
-                        .foregroundColor(.white).cornerRadius(10).padding()
+                        .foregroundColor(.white).cornerRadius(10)
+                        .padding([.leading,.trailing],20).padding(.top,10).font(.title3)
                 }
                 Spacer()
             }
         //}
+            .sheet(isPresented: $createQuestionPageIsShown) {
+                CreateQuestionAndAnswerScreen(isShown: $createQuestionPageIsShown, myQuestion: $question, myAnswer: $answer)
+            }
     }
 }
 
